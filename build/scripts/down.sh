d705d919f85c29127fbafc1191f7ecd321d4f8c4
Aaron Arruda <aaron@abarruda.com>
2021-08-21 17:01:26 -0700

    More robust route handling upon VPN disconnect.

--- build/scripts/down.sh
+++ build/scripts/down.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-echo "Killing transmission-daemon"
-kill $(pidof transmission-daemon)
+set -e
 
-#ip route add 1.1.1.1/32 via 10.44.0.0 dev eth0
+GATEWAY=${1}
+INTERFACE=${2}
+echo "GATEWAY: ${GATEWAY}"
+echo "INTERFACE: ${INTERFACE}"
+
+TRANSMISSION_PID=`pidof transmission-daemon`
+echo "`date` Killing transmission-daemon (pid: $TRANSMISSION_PID)"
+kill $TRANSMISSION_PID
+# wait until transmission exits before proceeding
+while kill -0 $TRANSMISSION_PID; do 
+    echo "`date` Waiting for transmission to exit..."
+    sleep 1
+done
+
+# add back the default route so reconnection can be made
+echo "`date` Configuring default routes"
+ip route add default via "${GATEWAY}" dev "${INTERFACE}"
--- build/scripts/up.sh
+++ build/scripts/up.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
+set -e
+
 GATEWAY=${1}
 INTERFACE=${2}
 VPN_SERVER=${3}
+echo "GATEWAY: ${GATEWAY}"
+echo "INTERFACE: ${INTERFACE}"
 
 echo "`date` TUNNEL UP!"
 
 # drop default route to avoid downloads from local IP
 ip r del default
 
-# Add route for local requests to UI
-ip route add 192.168.0.0/16 via "${GATEWAY}" dev "${INTERFACE}"
-
-# add a route only to the VPN server to allow for reconnections
-VPN_IP=$(cat /etc/openvpn/TorGuard."${VPN_SERVER}".ovpn | grep -oE "remote \b([0-9]{1,3}\.){3}[0-9]{1,3}\b"| awk 'NF {print $2}')
-ip route add "${VPN_IP}" via "${GATEWAY}" dev "${INTERFACE}"
+# Add route for local requests to UI.  Upon container restart, this will already 
+# be configured, so ignore errors
+ip route add 192.168.0.0/16 via "${GATEWAY}" dev "${INTERFACE}" || true
 
 # possibly limit transmission to only the VPN IP: https://gist.github.com/dmp1ce/6bcb38d1843bac1a25e9
 transmission-daemon --config-dir /etc/transmission --logfile /var/log/transmission.log
